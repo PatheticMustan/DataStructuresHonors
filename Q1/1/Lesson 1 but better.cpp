9992581a04c89f9b6b64a5cb8b032f9c0adfd106
Mustan Pathetic <patheticmustan@gmail.com>
2021-09-03 15:47:36 +0000

    lesson 1

---
+++ Q1/1/Lesson 1 but better.cpp
@@ -0,0 +1,12 @@
+/** Lesson 1 but better.cpp, by Kevin Wang
+ * prints "Hello, World!"
+ * all the indentation you've ever dreamed of
+ **/
+#include <iostream>
+
+int main()                                                                     {
+    std::cout << "Hello, World!" << "\n"                                       ;
+                                                                               ;
+    std::cout << "The big black bug" << std::endl                              ;
+    std::cout << "bled black blood!\n"                                         ;
+return 0                                                                       ;}
---
+++ Q1/1/Lesson 1.cpp
@@ -0,0 +1,13 @@
+/** Lesson 1.cpp, by Kevin Wang
+ * prints "Hello, World!"
+ * not sure why we need the filename in the header, if it's already in the filename
+ **/
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << "\n";
+
+    std::cout << "The big black bug" << std::endl;
+    std::cout << "bled black blood!\n";
+    return 0;
+}
