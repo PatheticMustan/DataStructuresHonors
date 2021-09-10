bf6e9f510c166424d11f070ffd0a328910608e11
Mustan Pathetic <patheticmustan@gmail.com>
2021-09-10 00:43:49 +0000

    build shit

--- Q1/3/Lesson3b.cpp
+++ Q1/3/Lesson3b.cpp
@@ -3,23 +3,21 @@
  * circle, where the user supplies the radius.
  **/
 
-// beautiful typedef, I'm such a funny person
+// the lazy #include
 #include <bits/stdc++.h>
 using namespace std;
 
 // reality can be whatever I want it to be
 // long double is the funniest type
-typedef double dooble;
+typedef long double dooble;
 
 int main() {
-    dooble pi = 3.1415;
-
     // a long double
     dooble radius;
     cout << "We're gonna do circle things, enter a radius" << endl;
     cin >> radius;
 
-    cout << "circum: " << 2 * pi * radius << endl;
-    cout << "circum: " << pi * radius * radius << endl;
+    cout << "circum: " << 2 * M_PI * radius << endl;
+    cout << "area: " << M_PI * radius * radius << endl;
     return 0;
 }
