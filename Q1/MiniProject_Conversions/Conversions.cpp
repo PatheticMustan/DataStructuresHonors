0aea9f9ed44e1ca98d1f4c921de3978ab78fa1f1
Mustan Pathetic <patheticmustan@gmail.com>
2021-09-16 19:08:30 +0000

    MP conversions comments

---
+++ Q1/MiniProject_Conversions/Conversions.cpp
@@ -0,0 +1,40 @@
+/** Conversions.cpp, by Kevin Wang
+ * 
+ * Complete the following functions with the given declarations. All user input and output must occur in your
+ * main function. You are permitted to create other functions if you feel they would help you complete the task.
+ * Call your C++ file Conversions.
+ * 
+ * This is to be submitted to Classroom by the date on the assignment. You are only completing one file for this mini-project.
+ * In your main function, you are going to be invoking the following functions. Obtain test values from the user
+ * through the cin command. Your program must contain enough input to properly invoke the following
+ * functions. The declarations are below:
+ * 
+ * string toBase(int decimal, int base)
+ *      Converts the argument integer from base 10 to the argument base (base <= 9).
+ * string toBase16(int decimal)
+ *      Converts the argument integer from base 10 to base 16.
+ * int toInteger(string number, int base)
+ *      Converts the argument string from the argument base to base 10 (base <= 9).
+ * int toInteger16(string number)
+ *      Converts the argument string from base 16 to base 10.
+ * 
+ * This project will be graded on:
+ * 1. Documentation (4 points): Your code must be fully commented and employ standard C++-style
+ *      conventions.
+ * 2. User-Friendliness (4 points): All interactions with the user must be clear and unambiguous.
+ * 3. Functions (16 points): Each function has the proper declaration and works as expected.
+ * TOTAL: 24 points
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    string toBase(int decimal, int base) {}
+    string toBase16(int decimal) {}
+    int toInteger(string number, int base)
+    int toInteger16(string number) {}
+
+    return 0;
+}
--- Q1/template.cpp
+++ Q1/template.cpp
@@ -2,15 +2,8 @@
  * does a thing
  **/
 
-// beautiful typedef, I'm such a funny person
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef int zinnt;
-typedef long long schlong;
-typedef string ericYachabes;
-typedef char tesla;
-typedef double dooble;
 
 int main() {
     
